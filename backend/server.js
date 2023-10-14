c512eb49e1ec1db63b458d35fdba355232579d5d
Jarvis <kamaleshselvam75@gmail.com>
2023-10-14 20:13:27 +0530

    added some logs,middlewares,cors

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,21 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3500;
 const path = require("path");
+const { logger } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
 
-app.use("/", express.static(path.join(__dirname, "public/")));
-app.use("/", require(path.join(__dirname, "routes/Root.Route")));
+//importing routes
+const RootRoute = require("./routes/Root.Route");
+app.use(logger);
+app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use(express.json());
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use("/", RootRoute);
+
 app.all("*", (req, res) => {
   res.status(404);
   if (req.accepts("html")) {
@@ -15,6 +27,8 @@ app.all("*", (req, res) => {
     res.type("txt").send("404 File Not Found");
   }
 });
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
